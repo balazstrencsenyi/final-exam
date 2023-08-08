365120720be71bea4396b8957f7a2964ce039439
balazstrencsenyi <b.trencsenyi@gmail.com>
2023-08-08 10:53:13 +0200

    almost finished

--- script.js
+++ script.js
@@ -11,22 +11,28 @@ headerTitle.setAttribute("class", "header-title");
 headerTitle.textContent = books.logo;
 headerContainer.appendChild(headerTitle);
 
-const headerButton = document.createElement("button");
+const headerButton = document.createElement("div");
 headerButton.setAttribute("class", "header-button");
 headerButton.textContent = books.button;
 headerContainer.appendChild(headerButton);
 
-function createCard(book) {
+function createCard(cardIndex, book) {
   const card = document.createElement("div");
   card.classList.add("card");
 
-  const title = document.createElement("h2");
-  title.textContent = book.title;
-  card.appendChild(title);
+  // Create a div for the card number
+  const cardNumber = document.createElement("div");
+  cardNumber.textContent = cardIndex + 1; // Adding 1 to the index to display 1-based numbering
+  cardNumber.classList.add("card-number");
+  card.appendChild(cardNumber);
 
   const subtitle = document.createElement("h3");
   subtitle.textContent = book.sub;
   card.appendChild(subtitle);
+
+  const title = document.createElement("h2");
+  title.textContent = book.title;
+  card.appendChild(title);
 
   const text = document.createElement("p");
   text.textContent = book.text;
@@ -36,14 +42,17 @@ function createCard(book) {
 }
 
 function populateCards() {
-  const root = document.getElementById("root");
+  const cardsContainer = document.createElement("div");
+  cardsContainer.setAttribute("class", "cards-container");
 
   if (books.cards) {
-    books.cards.forEach((book) => {
-      const card = createCard(book);
-      root.appendChild(card);
+    books.cards.forEach((book, index) => {
+      const card = createCard(index, book); // Pass the index to createCard
+      cardsContainer.appendChild(card);
     });
   }
+
+  root.appendChild(cardsContainer);
 }
 
 populateCards();
